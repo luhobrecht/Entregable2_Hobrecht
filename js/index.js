27a5f40a0a03e668d13750570143cb47862b5fe4
luhobrecht <luhobrecht@hotmail.com>
2024-07-03 19:55:00 +0200

    Added more functions

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-// Aseguramos que el script se ejecute después de que el DOM esté completamente cargado
 document.addEventListener('DOMContentLoaded', function () {
     // Variables globales
     const taskList = document.getElementById('taskList');
@@ -9,10 +8,14 @@ document.addEventListener('DOMContentLoaded', function () {
     let isRunning = false;
     let startCounter = 0;
 
+    // Cargar datos desde localStorage al iniciar
+    loadFromLocalStorage();
+
     // Event listener para agregar tarea
     document.getElementById('addTask').addEventListener('click', function () {
         const taskInput = document.getElementById('taskInput');
         const taskText = taskInput.value.trim();
+        console.log(taskText)
         if (taskText !== '') {
             const li = document.createElement('li');
             const checkbox = document.createElement('input');
@@ -20,12 +23,14 @@ document.addEventListener('DOMContentLoaded', function () {
             checkbox.classList = 'form-check-input'
             checkbox.addEventListener('change', function () {
                 li.classList.toggle('completed');
+                saveToLocalStorage();
             });
             li.appendChild(checkbox);
             li.classList = 'taskLi'
             li.appendChild(document.createTextNode(taskText));
             taskList.appendChild(li);
             taskInput.value = '';
+            saveToLocalStorage();
         }
     });
 
@@ -38,10 +43,12 @@ document.addEventListener('DOMContentLoaded', function () {
             li.appendChild(document.createTextNode(reminderText));
             li.addEventListener('click', function () {
                 li.classList.toggle('completed');
+                saveToLocalStorage();
             });
             li.classList = 'reminderLi btn btn-dark mb-2'
             reminderList.appendChild(li);
             reminderInput.value = '';
+            saveToLocalStorage();
         }
     });
 
@@ -62,7 +69,7 @@ document.addEventListener('DOMContentLoaded', function () {
             isRunning = false;
         }
         logTime();
-        elapsedTime = 0;
+        startCounter = 0; // Restablecer el contador
         timerDisplay.textContent = '00:00:00';
     });
 
@@ -80,14 +87,140 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     function logTime() {
+        const endTime = new Date();
+        const hours = endTime.getHours();
+        const minutes = endTime.getMinutes();
+        const seconds = endTime.getSeconds();
+        const formattedTime =
+            (hours < 10 ? '0' : '') + hours + ':' +
+            (minutes < 10 ? '0' : '') + minutes + ':' +
+            (seconds < 10 ? '0' : '') + seconds;
+
         const li = document.createElement('li');
-        li.textContent = timerDisplay.textContent;
+        li.textContent = `Terminado a las ${formattedTime} - Tiempo trabajado: ${timerDisplay.textContent}`;
         workedHours.appendChild(li);
+        saveToLocalStorage();
+    }
+
+    // MODALS
+    // Event listener para iniciar un nuevo contador desde modal
+    document.getElementById('startNewTimer').addEventListener('click', function () {
+        startCounter = 0;
+        timerDisplay.textContent = '00:00:00';
+        const timerModal = bootstrap.Modal.getInstance(document.getElementById('timerModal'));
+        timerModal.hide();
+    });
+
+    // Event listener para agregar tarea desde modal
+    document.getElementById('modalAddTask').addEventListener('click', function () {
+        const taskInput = document.getElementById('modalTaskInput');
+        const taskText = taskInput.value.trim();
+        if (taskText !== '') {
+            const li = document.createElement('li');
+            const checkbox = document.createElement('input');
+            checkbox.type = 'checkbox';
+            checkbox.classList = 'form-check-input'
+            checkbox.addEventListener('change', function () {
+                li.classList.toggle('completed');
+                saveToLocalStorage();
+            });
+            li.appendChild(checkbox);
+            li.classList = 'taskLi'
+            li.appendChild(document.createTextNode(taskText));
+            taskList.appendChild(li);
+            taskInput.value = '';
+            // Cerrar el modal
+            const taskModal = bootstrap.Modal.getInstance(document.getElementById('taskModal'));
+            taskModal.hide();
+            saveToLocalStorage();
+        }
+    });
+
+    // Event listener para agregar recordatorio desde modal
+    document.getElementById('modalAddReminder').addEventListener('click', function () {
+        const reminderInput = document.getElementById('modalReminderInput');
+        const reminderText = reminderInput.value.trim();
+        if (reminderText !== '') {
+            const li = document.createElement('li');
+            li.appendChild(document.createTextNode(reminderText));
+            li.addEventListener('click', function () {
+                li.classList.toggle('completed');
+                saveToLocalStorage();
+            });
+            li.classList = 'reminderLi btn btn-dark mb-2'
+            reminderList.appendChild(li);
+            reminderInput.value = '';
+            // Cerrar el modal
+            const reminderModal = bootstrap.Modal.getInstance(document.getElementById('reminderModal'));
+            reminderModal.hide();
+            saveToLocalStorage();
+        }
+    });
+
+    // LOCAL STORAGE FUNCTIONS
+    // Función para guardar datos en localStorage
+    function saveToLocalStorage() {
+        const tasks = [];
+        taskList.querySelectorAll('li').forEach(task => {
+            tasks.push({ text: task.textContent, completed: task.classList.contains('completed') });
+        });
+        const reminders = [];
+        reminderList.querySelectorAll('li').forEach(reminder => {
+            reminders.push({ text: reminder.textContent, completed: reminder.classList.contains('completed') });
+        });
+        const workedHoursList = [];
+        workedHours.querySelectorAll('li').forEach(workedHour => {
+            workedHoursList.push(workedHour.textContent);
+        });
+
+        const data = {
+            tasks: tasks,
+            reminders: reminders,
+            workedHours: workedHoursList
+        };
+        localStorage.setItem('tasklyData', JSON.stringify(data));
+    }
+
+    // Función para cargar datos desde localStorage
+    function loadFromLocalStorage() {
+        const data = JSON.parse(localStorage.getItem('tasklyData'));
+        if (data) {
+            data.tasks.forEach(task => {
+                const li = document.createElement('li');
+                const checkbox = document.createElement('input');
+                checkbox.type = 'checkbox';
+                checkbox.classList = 'form-check-input'
+                checkbox.checked = task.completed;
+                checkbox.addEventListener('change', function () {
+                    li.classList.toggle('completed');
+                    saveToLocalStorage();
+                });
+                li.appendChild(checkbox);
+                li.classList = 'taskLi'
+                li.appendChild(document.createTextNode(task.text));
+                if (task.completed) {
+                    li.classList.add('completed');
+                }
+                taskList.appendChild(li);
+            });
+            data.reminders.forEach(reminder => {
+                const li = document.createElement('li');
+                li.appendChild(document.createTextNode(reminder.text));
+                li.addEventListener('click', function () {
+                    li.classList.toggle('completed');
+                    saveToLocalStorage();
+                });
+                li.classList = 'reminderLi btn btn-dark mb-2';
+                if (reminder.completed) {
+                    li.classList.add('completed');
+                }
+                reminderList.appendChild(li);
+            });
+            data.workedHours.forEach(workedHour => {
+                const li = document.createElement('li');
+                li.textContent = workedHour;
+                workedHours.appendChild(li);
+            });
+        }
     }
 });
-
-// Función para agregar tareas desde acceso directo en navbar
-document.getElementById('newTask').addEventListener('click', function () {
-    const addTask = prompt('Agrega una tarea nueva.')
-    taskList += addTask
-})
